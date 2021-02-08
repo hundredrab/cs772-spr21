b84808223f34afe94af9dbf134c4185094280e38
hundredrab <jha.sourab@gmail.com>
2021-02-08 17:13:51 +0530

    Decent model with ~.6 accuracy

--- assign1/code.py
+++ assign1/code.py
@@ -23,6 +23,7 @@ def vocab_dict(text):
     uniq = set()
     text.apply(uniq.update)
     int_to_word = dict(enumerate(uniq))
+    int_to_word[len(int_to_word)] = '<UNK>'
     word_to_int = {v: k for k, v in int_to_word.items()}
     return int_to_word, word_to_int
 
@@ -79,20 +80,15 @@ def preprocess_data(data, vdic=None):
     """Preprocesses data using the above defined functions."""
     data = convert_to_lower(data)
     data = remove_punctuation(data)
-    import pdb
 
     data = perform_tokenization(data)
-    import pdb
 
     data = remove_stopwords(data)
-    import pdb
 
     _, w2i = vocab_dict(data)
-    data = encode_data(data, w2i)
-    import pdb
+    # data = encode_data(data, w2i)
 
-    data = perform_padding(data)
-    import pdb
+    # data = perform_padding(data)
 
 
     return data, w2i
@@ -127,28 +123,31 @@ def softmax_activation(x):
 
 
 class NeuralNet(nn.Module):
-    def __init__(self, reviews, ratings):
+    def __init__(self, num_features):
         super().__init__()
-        self.reviews = reviews.float()
-        self.ratings = ratings
-        self.build_nn()
+        # self.reviews = reviews.float()
+        # self.ratings = ratings
+        self.build_nn(num_features)
 
-    def build_nn(self):
-        self.fc1 = nn.Linear(30, 5)
+    def build_nn(self, num_features):
+        print(num_features, 98765)
+        self.fc1 = nn.Linear(num_features, 5)
 
     def forward(self, x):
         x = self.fc1(x)
         x = softmax_activation(x)
         return x
 
-    def train(self):#, batch_size, epochs):
+    def train(self, train_df):#, batch_size, epochs):
         from torch.optim import Adam
 
-        for i in range(50):
-            optm = Adam(self.parameters(), lr=0.003)
+        for i in range(2000):
+            batch = train_df.groupby('ratings').apply(lambda x: x.sample(16))
+            # import pdb; pdb.set_trace();
+            optm = Adam(self.parameters(), lr=0.001)
             self.zero_grad()
-            x = self.reviews
-            y = self.ratings
+            x = torch.tensor(np.matrix(batch.reviews.tolist())).float()
+            y = torch.tensor(batch.ratings.tolist())
             output = self(x)
             criterion = nn.CrossEntropyLoss()
             loss = criterion(output.float(), y)
@@ -159,6 +158,7 @@ class NeuralNet(nn.Module):
 
     def predict(self, reviews):
         print(type(reviews))
+        reviews = torch.tensor(np.matrix(reviews.tolist()))
         with torch.no_grad():
             out = self.forward(reviews.float())
             print(out)
@@ -169,6 +169,9 @@ class NeuralNet(nn.Module):
 # DO NOT MODIFY MAIN FUNCTION'S PARAMETERS
 def main(train_file, test_file):
     from sklearn.model_selection import train_test_split
+    from keras.utils import to_categorical
+    from sklearn.feature_extraction.text import CountVectorizer
+
 
     batch_size, epochs = 1, 2
     train = pd.read_csv(train_file)
@@ -177,17 +180,44 @@ def main(train_file, test_file):
     train_ratings = train["ratings"] - 1
     test_reviews = test["reviews"]
 
-    train_reviews, vocab_dict = preprocess_data(train_reviews)
-    test_reviews, _ = preprocess_data(test_reviews, vocab_dict)
+    # train_reviews, vocab_dict = preprocess_data(train_reviews)
+    train_df = pd.concat([train_reviews, train_ratings], axis=1)
+    train_df, validation_df = train_test_split(train_df, random_state=42)
+    vectorizer = CountVectorizer(min_df=2, stop_words=stopwords.words('english'))
+    train_df.reviews = vectorizer.fit_transform(train_df.reviews).toarray().tolist()
+    validation_df.reviews = vectorizer.transform(validation_df.reviews).toarray().tolist()
+    X_test = pd.Series(vectorizer.transform(test_reviews).toarray().tolist())
+    # y = train_df.ratings
+    # y_val = validation_df.ratings
+    # import pdb; pdb.set_trace();
 
 
-    model = NeuralNet(torch.tensor(np.matrix(train_reviews.to_list())), torch.tensor(train_ratings.values))
+    # h1_enc = OneHotEncoder(handle_unknown='ignore')
+    # import pdb; pdb.set_trace();
+    # h1_enc.fit(train_reviews.tolist())
+    # import pdb; pdb.set_trace();
+
+
+
+    # h1_enc.transform(train_reviews)
+    # train_reviews = train_reviews.apply(lambda x: to_categorical(x))
+    # import pdb; pdb.set_trace();
+    # test_reviews, _ = preprocess_data(test_reviews, vocab_dict)
+
+
+    model = NeuralNet(len(vectorizer.get_feature_names()))
     # model.build_nn()
     # model.train_nn(batch_size, epochs)
-    model.train()
-    print(torch.tensor(np.matrix(test_reviews.to_list())))
-    predictions = (model.predict(torch.tensor(np.matrix(test_reviews.to_list())))).tolist()
-    from collections import Counter 
+    model.train(train_df)
+    # print(torch.tensor(np.matrix(test_reviews.to_list())))
+    print('hii')
+    # import pdb; pdb.set_trace()
+    predictions = model.predict(validation_df.reviews).tolist()#(model.predict(torch.tensor(np.matrix(test_reviews.to_list())))).tolist()
+    correct = (sum(np.array(predictions) == np.array(validation_df.ratings)))
+    print(len(predictions))
+    print(f"Accuracy: {correct/len(predictions)}")
+    from collections import Counter
+    # import pdb; pdb.set_trace()
     print(Counter(predictions))
 
-    return model.predict(test_reviews)
+    return
