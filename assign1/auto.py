d66836d4bccbdda628d5a54fdfc7b22dd036a28f
hundredrab <jha.sourab@gmail.com>
2021-02-21 14:48:00 +0530

    Final assignment1 submission

--- assign1/auto.py
+++ assign1/auto.py
@@ -1,8 +1,10 @@
-from code import *
+from assign1 import *
 
 
 
 
 if __name__ == "__main__":
+    gold = pd.read_csv('gold_test.csv')
+    print(gold.ratings.tolist())
     val=main("train.csv","test.csv")
     print(val)
